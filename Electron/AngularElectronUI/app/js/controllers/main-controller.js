4d2c7242300cd5bfdf2c2db6220acbc111c6623d
asincrono <asincrono@gmail.com>
2016-06-06 19:01:20 +0200

    Bitrate showed in Bps instead of Mbps solved!
    Another history of success!

--- Electron/AngularElectronUI/app/js/controllers/main-controller.js
+++ Electron/AngularElectronUI/app/js/controllers/main-controller.js
@@ -571,7 +571,6 @@ angular.module('MainApp')
 
       function checkBitrate(callback) {
         NetInfo.getRx($scope.selectedDevice, (err, bytes, timestamp) => {
-          console.log('(mainCtrl) at checkBitrate.')
           if (err) {
             console.log(err)
           } else {
@@ -579,7 +578,7 @@ angular.module('MainApp')
               if ($scope.rxStats) {
                 console.log('(mainCtrl) checkBitrate rxStats:', $scope.rxStats)
                 let elapsedTime = timestamp - $scope.rxStats.timestamp
-                console.log('(mainCtrl) elapsedTime:', elapsedTime)
+                console.log('(mainCtrl) elapsedTime:', Math.trunc(ElapsedTime / 1000))
                 let receivedBytes = (bytes - $scope.rxStats.bytes)
                 console.log('(mainCtrl) receivedBytes:', receivedBytes)
                 // we read bytes we need to turn in bits * 8 (-> bps)
